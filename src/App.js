ce35dd12a5a5ab4aa069f5612bd48c186b0e6683
Robert Bristow <robertbristow3@gmail.com>
2021-11-15 09:57:36 +0000

    fix style file paths and comment out store in App for now until it is functional

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import React, { Component } from "react";
 import { Greeting } from "./components/Greeting";
 import { Provider } from "react-redux";
-import store from "./store";
+// import store from "./store";
+import { Home, Questions, Categories, Scores } from './pages';
 
 function App() {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
+    <>
       <Greeting />
       <h1>This is the app</h1>
-    </Provider>
+    </>
+    // </Provider>
   );
 }
 export default App;
