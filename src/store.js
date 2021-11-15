22b71196e634bc3b100bca13c595684cfdec44c7
“Rachiey” <rachaelyonoff@gmail.com>
2021-11-15 14:50:34 +0000

    added a provider, commented out xxxReducer and added stuff to store.js

--- src/store.js
+++ src/store.js
@@ -1,21 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { devToolsEnhancer } from "redux-devtools-extension";
 
+import questionReducer from "./reducers/questionReducer";
+const store = createStore(questionReducer, composeWithDevTools(applyMiddleware(thunk)));
+// const initialState = {};
 
-const initialState = {};
+// const middleware = [thunk];
 
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState
-  // compose(
-  //   applyMiddleware(...middleware),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  //     window.__REDUX__DEVTOOLS_EXTENSION__()
-  // )
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState
+//   // compose(
+//   //   applyMiddleware(...middleware),
+//   //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//   //     window.__REDUX__DEVTOOLS_EXTENSION__()
+//   // )
+// );
 
 export default store;
