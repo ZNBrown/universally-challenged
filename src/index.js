ccbd94e23035b8a20099981c172f36aaa0f2e608
Max Mackie <maxmackie13@gmail.com>
2021-11-16 09:26:52 +0000

    adding redux back in

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./style.css";
 import App from "./App";
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-  <Router>
-    <App />
-  </Router>,
-  </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 
   document.getElementById("root")
 );
