9bad629d0e2ebed9dd7808da6e0cdea40e50a67a
Max Mackie <maxmackie13@gmail.com>
2021-11-16 11:28:49 +0000

    updated timer

--- src/pages/QuestionsPage/index.js
+++ src/pages/QuestionsPage/index.js
@@ -3,22 +3,20 @@ import { useSelector, useDispatch } from "react-redux"; // New imports to work w
 import "./style.css";
 import { scrubStr, shuffle, resetState, submitAnswer } from "../../actions";
 import { useHistory } from "react-router";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import axios from "axios";
 import Countdown from "react-countdown";
+import { Timer } from "../../components";
 
 const QuestionsPage = () => {
-    const [key, setKey] = useState(0);
-    const [countdownKey, setCountdownKey] = useState(0);
-    const username = useSelector((state) => state.username);
-    const difficulty = useSelector((state) => state.difficulty);
-    const currentScore = useSelector((state) => state.score);
-    const currentQuestionIndex = useSelector((state) => state.questionIndex);
-    let results = useSelector((state) => state.result);
-    const history = useHistory();
-    const dispatch = useDispatch();
-
-  
+  const [key, setKey] = useState(0);
+  const [countdownKey, setCountdownKey] = useState(0);
+  const username = useSelector((state) => state.username);
+  const difficulty = useSelector((state) => state.difficulty);
+  const currentScore = useSelector((state) => state.score);
+  const currentQuestionIndex = useSelector((state) => state.questionIndex);
+  let results = useSelector((state) => state.result);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const submitData = () => {
     console.log("Submit Data is calling");
@@ -62,8 +60,7 @@ const QuestionsPage = () => {
     ]);
 
     return (
-      <div
-        role='questionPage'>
+      <div role="questionPage">
         <Countdown date={Date.now() + 5000} key={countdownKey}>
           <div>
             <div>
@@ -73,38 +70,18 @@ const QuestionsPage = () => {
 
             <div>
               <h1>
-                <CountdownCircleTimer
-                  onComplete={() => {
-                    setCountdownKey((prevCountdownKey) => prevCountdownKey + 1);
-                    dispatch(submitAnswer(""));
-                    return [true, 100];
-                  }}
-                  key={key}
-                  isPlaying
-                  duration={15}
-                  colors={[
-                    ["#64DFDF", 0.25],
-                    ["#48BFE3", 0.25],
-                    ["#5E60CE", 0.25],
-                    ["#6930C3", 0.25],
-                  ]}
-                >
-                  {({ remainingTime }) => remainingTime}
-                </CountdownCircleTimer>
+                <Timer />
               </h1>
             </div>
 
             <br></br>
             <div>
-              <p>
-                {" "}
-                {scrubStr(results[currentQuestionIndex].question)}{" "}
-              </p>
+              <p> {scrubStr(results[currentQuestionIndex].question)} </p>
               <div>
                 {answers.map((t, i) => (
                   <button
-                    role='button'
-                    name='answerButton'
+                    role="button"
+                    name="answerButton"
                     id={i}
                     onClick={sendAnswer}
                     value={t}
@@ -129,7 +106,10 @@ const QuestionsPage = () => {
           <h3>Final Score: {currentScore} /10 </h3>
           <br></br>
           <h5>
-            <i>Scores will be adjusted with a multiplier of 1.6 for "hard" and 1.3 for "medium" quiz</i>
+            <i>
+              Scores will be adjusted with a multiplier of 1.6 for "hard" and
+              1.3 for "medium" quiz
+            </i>
           </h5>
           <br></br>
 
