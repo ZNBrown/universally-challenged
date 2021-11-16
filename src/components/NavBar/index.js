c4a7f2d4c2f6536111d1c0ee5a68441199d76bad
Robert Bristow <robertbristow3@gmail.com>
2021-11-16 12:13:46 +0000

    add ref to nav bar and nav buttong for DOM manipulation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { BackButton } from "../BackButton";
 import "./style.css";
 
 const NavBar = () => {
+  const navBtn = useRef();
+  const navBar = useRef();
+
+
+  const showNav = () => {
+    switch (navBtn.current.id) {
+      case 'nav-btn-burger':
+        navBtn.current.innerHTML = '&times;';
+        navBtn.current.id = 'nav-btn-cross';
+        navBar.current.id = 'show';
+        break;
+      case 'nav-btn-cross':
+        navBtn.current.innerHTML = '&#9776;';
+        navBtn.current.id = 'nav-btn-burger';
+        navBar.current.id = 'hide';
+        break;
+    }
+  }
+
   return (
-    <nav className="links-container" role="navigation">
-      <NavLink className="links" to="/">
-        Home
-      </NavLink>
-      <NavLink className="links" to="/Leaderboard">
-        Leaderboard
-      </NavLink>
-      <NavLink className="links" to="/categories">
-        New Quiz
-      </NavLink>
-      <NavLink className="links" to="/about">
-        About
-      </NavLink>
-      <BackButton />
-    </nav>
+    <>
+      <div ref= {navBtn} id='nav-btn-burger' onClick={showNav}>&#9776;</div>
+      <nav ref={navBar} id='hide' className="links-container" role="navigation">
+        <NavLink className="links" to="/">
+          Home
+        </NavLink>
+        <NavLink className="links" to="/Leaderboard">
+          Leaderboard
+        </NavLink>
+        <NavLink className="links" to="/categories">
+          New Quiz
+        </NavLink>
+        <NavLink className="links" to="/about">
+          About
+        </NavLink>
+        <BackButton />
+      </nav>
+    </>
   );
 };
 
