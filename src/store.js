d20642594d56271e61ac07bc3658ccbfddd8f339
“Rachiey” <rachaelyonoff@gmail.com>
2021-11-16 12:15:45 +0000

    fixing errors

--- src/store.js
+++ src/store.js
@@ -9,18 +9,18 @@ const store = createStore(
   questionReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
-const initialState = {};
+// const initialState = {};
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX__DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX__DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
