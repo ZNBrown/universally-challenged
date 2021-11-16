8484008ed31b7ce4a7665206f4670a7fe33af532
ZNBrown <ZachNatBrown@gmail.com>
2021-11-16 10:51:34 +0000

    updates dbconfig

--- dbConfig/init.js
+++ dbConfig/init.js
@@ -1,5 +1,13 @@
 const { Pool } = require("pg");
-
-const pool = new Pool();
+let config;
+if (process.env.DATABASE_URL){
+config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+}
+const pool = new Pool(config);
 
 module.exports = pool;
