975d0ec87d57aa2a35d4411229935cd116e0147b
ZNBrown <ZachNatBrown@gmail.com>
2021-11-17 12:14:04 +0000

    hopefully fixes deploy

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const ROOT_DIRECTORY = path.join(__dirname, "../"); // the root of your project
-const PUBLIC_DIRECTORY = path.join(ROOT_DIRECTORY, "public"); // the root of the frontend, i.e. html file
+const ROOT_DIRECTORY = path.join(__dirname, '../'); // the root of your project
+const PUBLIC_DIRECTORY = path.join(ROOT_DIRECTORY, 'public'); // the root of the frontend, i.e. html file
 
 const config = {
-  entry: [path.resolve(ROOT_DIRECTORY, "src/index.js")], // the main JavaScript file of the project
+  entry: [path.resolve(ROOT_DIRECTORY, 'src/index.js')], // the main JavaScript file of the project
   output: {
     // instructions for compiling the code
-    path: path.resolve(ROOT_DIRECTORY, "build"), // the file where the compiled code should go
-    filename: "bundle.js", // the file name of the compiled code
-    publicPath: "/", // specifies the base path for all the assets within your application.
+    path: path.resolve(ROOT_DIRECTORY, 'build'), // the file where the compiled code should go
+    filename: 'bundle.js', // the file name of the compiled code
+    publicPath: '/', // specifies the base path for all the assets within your application.
   },
-  mode: "development", // tells webpack to use its built-in optimizations according to the mode
+  mode: 'development', // tells webpack to use its built-in optimisations according to the mode
   resolve: {
     // instructions on how to resolve modules
-    modules: [path.resolve("node_modules"), "node_modules"], // tells webpack where to look for node_modules
+    modules: [path.resolve('node_modules'), 'node_modules'], // tells webpack where to look for node_modules
   },
   performance: {
     // notifies you if assets and entry points exceed a specific file limit
@@ -25,42 +25,29 @@ const config = {
     // plugins we are using to help with compiling
     new HtmlWebpackPlugin({
       // used to add the JavaScript code to the HTML
-      template: path.join(PUBLIC_DIRECTORY, "index.html"),
+      template: path.join(PUBLIC_DIRECTORY, 'index.html'),
     }),
   ],
   module: {
     // helpers we want webpack to use
     rules: [
-      {
+      // specific instructions for each helper
+      { 
         test: /\.(js|jsx)$/,
         resolve: {
-          extensions: [".js", ".jsx"],
-        },
-        exclude: /nodeModules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+          extensions: [".js", ".jsx"]
+        }, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader' 
+      }, // transpile JavaScript files
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       }, // transpile css files
       {
         test: /\.(png|svg|jpg|gif|pdf)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       }, // transpile image files
-
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
     ],
   },
 };
