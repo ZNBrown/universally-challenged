67f990d3033349e072301575f8cd949fe667a137
Robert Bristow <robertbristow3@gmail.com>
2021-11-18 17:48:36 +0000

    add test coveraeg

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { HomepageSelector } from "./HomepageSelector";
 export { BackButton } from "./BackButton";
 export { NavBar } from "./NavBar";
 export { EntryForm } from "./EntryForm";
-export { Timer } from "./Timer";
 export { Timer321 } from "./Timer-321";
 export { CollapsibleCard } from "./CollapsibleCard";
 export { LeaderboardItem } from "./LeaderboardItem";
